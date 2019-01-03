a79fa0561ea7e4f92f1961f1ea257f0ada2ff3a9
Ilian Ivanov <noodle.hell@gmail.com>
2019-01-03 18:47:44 +0200

    integrated with sockets

--- Front-end-React/src/App.js
+++ Front-end-React/src/App.js
@@ -2,39 +2,51 @@ import React, { Component } from 'react';
 import socketIOClient from "socket.io-client";
 import './App.css';
 
-const span = React.createElement('span', {}, "zdre")
+const host = "10.10.0.239" // must be the real ip to work on phones otherwise localhost does the
+let room = 'chat'
+const endpoint = `http://${host}:3001/${room}`;
+const socket = socketIOClient(endpoint);
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       response: false,
-      endpoint: "http://localhost:3001",
+      endpoint: `http://localhost:3001/${room}`,
       screenName:'',
       itemArray: [],
+      check: false,
       msg:''
     }
   }
   componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
+    // const { endpoint } = this.state;
+    // const socket = socketIOClient(endpoint);
     socket.on("data", data => {
       this.setState({
         response: data
       })
     }
     );
+  socket.on('user', user => {
+      // console.log(user)
+      this.setState({
+        itemArray: user
+      })
+    })
   }
   createElement = (event) => {
-    if (event.target.getAttribute('name') === 'screenName'){
+    if (event.target.getAttribute('name') === 'screenName' || event.target.getAttribute('name') === 'room'){
       return
     }
-    console.log("X ===", event.clientX)
-    console.log("Y ===", event.clientY)
+
+    // console.log("X ===", event.clientX)
+    // console.log("Y ===", event.clientY)
     const items = this.state.itemArray
     const positionX = event.clientX
     const positionY = event.clientY
-    // const name = "test"
+
     let posObj = {
       x: positionX,
       y: positionY,
@@ -43,34 +55,47 @@ class App extends Component {
     }
     items.push(posObj)
     this.setState({
-      itemArray: items
+      itemArray: items,
+      check: false,
+      msg:''
     })
   }
 
-  handleChange = (e) => {
+  handleChange = async (e) => {
     let arrtobeimported = this.state.itemArray
     this.setState({
       [e.target.name]: e.target.value,
-      msg: e.target.value
-    })
+      msg: e.target.value,
+    });
     if (arrtobeimported.length > 0){
-        arrtobeimported[arrtobeimported.length - 1].msg = this.state.msg
+      console.log("zdre")
+      arrtobeimported[arrtobeimported.length - 1].msg = this.state.msg
       }
+    var user = this.state.itemArray
+    socket.on('user', user => {
+      this.setState({
+        itemArray: user,
+      })
+    })
+    socket.emit('user', user);
+    console.log(this.state.itemArray)
+
   }
 
   componentWillMount() {
     document.addEventListener('click', this.createElement)
   }
   render() {
+    // console.log(this.state.itemArray)
     const { response } = this.state;
     return (
       <div className="App">
         <h1>{response ? "CONNECTED" : "NOT CONNECTED"}</h1>
         <p><input className="EnterName" onChange={this.handleChange} name="screenName" placeholder="Enter Name" value={this.state.screenName}/></p>
         {this.state.itemArray.map((res, index) => {
+          console.log('res', res.msg)
           if (res.x !== undefined) {
-            console.log(res.msg)
-            return <span className="pos" key={index} style={{ top: res.y, left: res.x }}>{res.name}:<input className="input" autoFocus onChange={this.handleChange} name={res.name} /></span>
+            return <span className="pos" key={index} style={{ top: res.y, left: res.x }} onChange={this.handleChange} name={res.name}>{res.name}:<input className="input" autoFocus />{res.msg}</span>
           }
         })}
       </div>
--- server.js
+++ server.js
@@ -2,18 +2,25 @@ const port = 3001;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var chat = io.of('/chat')
 app.get('/', function (req, res) {
     res.send("HELLO");
 });
-
-io.on('connection', function (socket) {
-        console.log("NEW USER")
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-    socket.emit("data", true);
-});
+chat.on('connection', function(socket){
+    chat.emit('data', true)
+    socket.on('user', user => {
+        console.table(user)
+        chat.emit("user", user)
+    })
+})
+chat.emit('hi', 'everyone')
+// io.on('connection', function (socket) {
+//         console.log("NEW USER")
+//     socket.on('disconnect', function () {
+//         console.log('user disconnected');
+//     });
+//     socket.emit("data", true);
+// });
 
 http.listen(port, function () {
     console.log(`listening on :${port}`);
