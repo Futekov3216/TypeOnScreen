8c2efcb3780011d20ba1294f28bde5175e2ad6cd
Ilian Ivanov <noodle.hell@gmail.com>
2019-01-04 12:04:24 +0200

    added modal for chat room and fix for chat inputs

--- Front-end-React/src/App.js
+++ Front-end-React/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import socketIOClient from "socket.io-client";
 import './App.css';
+import { Button, Modal } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
 
 const host = "10.10.0.239" // must be the real ip to work on phones otherwise localhost does the
 let room = 'chat'
 const endpoint = `http://${host}:3001/${room}`;
 const socket = socketIOClient(endpoint);
+
 
 
 class App extends Component {
@@ -16,11 +19,28 @@ class App extends Component {
       endpoint: `http://localhost:3001/${room}`,
       screenName:'',
       itemArray: [],
-      check: false,
-      msg:''
+      open: true,
+      msg:'',
+      fontSize:16,
+      element1:'',
+      element2:''
     }
   }
+
+  //  MODAL CONTROLS 
+  show = size => () => this.setState({ size, open: true })
+  close = () => this.setState({ open: false })
+
+
   componentDidMount() {
+    console.log(document.getElementsByClassName('modal'))
+    const modal1 = document.getElementsByClassName('modals')[0]
+    const modal2 = document.getElementsByClassName('modal')[0]
+    this.setState({
+      element: modal1,
+      element2: modal2
+    })
+    // console.log(modal)
     // const { endpoint } = this.state;
     // const socket = socketIOClient(endpoint);
     socket.on("data", data => {
@@ -37,7 +57,9 @@ class App extends Component {
     })
   }
   createElement = (event) => {
-    if (event.target.getAttribute('name') === 'screenName' || event.target.getAttribute('name') === 'room'){
+    console.log(event.target)
+    console.log(this.state.element)
+    if (event.target.getAttribute('name') === 'screenName' || event.target.getAttribute('name') === 'room' || event.target === this.state.element){
       return
     }
 
@@ -51,13 +73,14 @@ class App extends Component {
       x: positionX,
       y: positionY,
       name: this.state.screenName === '' ? 'Anonymous' : this.state.screenName,
-      msg:''
+      msg:'',
+      fontSize: (Math.floor(Math.random() * 21) + 13)
     }
     items.push(posObj)
     this.setState({
       itemArray: items,
-      check: false,
-      msg:''
+      msg:'',
+      fontSize: 16
     })
   }
 
@@ -66,19 +89,18 @@ class App extends Component {
     this.setState({
       [e.target.name]: e.target.value,
       msg: e.target.value,
+    }, () => {
+      if (arrtobeimported.length > 0){
+        arrtobeimported[arrtobeimported.length - 1].msg = this.state.msg
+        var user = this.state.itemArray
+          socket.on('user', user => {
+            this.setState({
+              itemArray: user,
+            })
+          })
+        socket.emit('user', user);
+        }
     });
-    if (arrtobeimported.length > 0){
-      console.log("zdre")
-      arrtobeimported[arrtobeimported.length - 1].msg = this.state.msg
-      }
-    var user = this.state.itemArray
-    socket.on('user', user => {
-      this.setState({
-        itemArray: user,
-      })
-    })
-    socket.emit('user', user);
-    console.log(this.state.itemArray)
 
   }
 
@@ -87,15 +109,27 @@ class App extends Component {
   }
   render() {
     // console.log(this.state.itemArray)
-    const { response } = this.state;
+    const { response, size, open } = this.state;
     return (
       <div className="App">
+        <Modal size={size} open={open}>
+          <Modal.Header>Delete Your Account</Modal.Header>
+          <Modal.Content>
+            <p>Are you sure you want to delete your account</p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button negative>No</Button>
+            <Button positive icon='checkmark' labelPosition='right' content='Yes' />
+          </Modal.Actions>
+        </Modal>
         <h1>{response ? "CONNECTED" : "NOT CONNECTED"}</h1>
         <p><input className="EnterName" onChange={this.handleChange} name="screenName" placeholder="Enter Name" value={this.state.screenName}/></p>
         {this.state.itemArray.map((res, index) => {
-          console.log('res', res.msg)
-          if (res.x !== undefined) {
-            return <span className="pos" key={index} style={{ top: res.y, left: res.x }} onChange={this.handleChange} name={res.name}>{res.name}:<input className="input" autoFocus />{res.msg}</span>
+          // console.log('res', res.msg)
+          if (res.x === undefined) {
+            return null
+          }else{
+            return <span className="pos" key={index} style={{ top: res.y, left: res.x, fontSize: res.fontSize }} onChange={this.handleChange} name={res.name}>{res.name}:<input className="input" autoFocus />{res.msg}</span>
           }
         })}
       </div>
